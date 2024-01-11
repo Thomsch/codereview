fde32ff4e7088bdb0a5c409535990c0e830f2148
Thomas Schweizer <thms.sch@gmail.com>
2024-01-10 17:12:51 -0800

    Remove Vue links

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
   <ApiExample/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 import ApiExample from "./components/ApiExample.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld, ApiExample
+    ApiExample
   }
 }
 </script>
