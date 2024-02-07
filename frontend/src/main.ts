ba5aaaffcdb1b82f065aa8c32a3626f9637e56d1
Thomas Schweizer <thms.sch@gmail.com>
2024-02-06 22:40:29 -0800

    Add speed insights

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
 inject();
+injectSpeedInsights();
 
 createApp(App).mount('#app')
